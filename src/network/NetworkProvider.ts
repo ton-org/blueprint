c2d1aaf4c7f23e8769c7275ac2cc33100f45f0c4
krigga <krigga7@gmail.com>
2023-02-17 13:39:24 +0300

    feat: add waitForDeploy, deprecate deploy on NetworkProvider

--- src/network/NetworkProvider.ts
+++ src/network/NetworkProvider.ts
@@ -7,6 +7,12 @@ export interface NetworkProvider {
     sender(): Sender;
     api(): TonClient;
     provider(addr: Address): ContractProvider;
+    waitForDeploy(address: Address, attempts?: number, sleepDuration?: number): Promise<void>;
+    /**
+     * @deprecated
+     *
+     * Use your Contract's `sendDeploy` method (or similar) together with `waitForDeploy` instead.
+     */
     deploy(contract: Contract, value: bigint, body?: Cell, waitAttempts?: number): Promise<void>;
     open<T extends Contract>(contract: T): OpenedContract<T>;
     ui(): UIProvider;
