463fb44c389d4169fdfb428d4af94450378dee5e
Aliaksandr Bahdanau <a.bahdanau@pixelplex.io>
2024-06-19 15:07:41 +0300

    Add test args

--- src/cli/test.ts
+++ src/cli/test.ts
@@ -1,6 +1,7 @@
 import { Runner } from './Runner';
 import { execSync } from 'child_process';
 
-export const test: Runner = async () => {
-    execSync('npm test', { stdio: 'inherit' });
+export const test: Runner = async (args) => {
+    const testArgs = args._.slice(1); // first argument is `test`, need to get rid of it
+    execSync(`npm test ${testArgs.join(' ')}`, { stdio: 'inherit' });
 };
