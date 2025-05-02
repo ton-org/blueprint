71df052c44f85075ea499ce58ad5d2f370be5164
Alejandbel <bogdanov28c@gmail.com>
2025-05-02 16:46:40 +0300

    fix: fix regex

--- src/utils/string.utils.ts
+++ src/utils/string.utils.ts
@@ -5,7 +5,7 @@ export function isPascalCase(str: string): boolean {
 export function toPascalCase(str: string): string {
     return str
         .replace(/([a-z])([A-Z])/g, '$1 $2') // Splits camelCase words into separate words
-        .replace(/[-_]+|[^\p{L}\p{N}]/gu, ' ') // Replaces dashes, underscores, and special characters with spaces
+        .replace(/[^a-zA-Z0-9]+/g, ' ') // Replaces all characters that are not allowed with spaces
         .toLowerCase() // Converts the entire string to lowercase
         .replace(/(?:^|\s)(\p{L})/gu, (_, letter) => letter.toUpperCase()) // Capitalizes the first letter of each word
         .replace(/\s+/g, ''); // Removes all spaces
