338a4310da07c6de129b3084d23ac52388b75605
Alejandbel <bogdanov28c@gmail.com>
2025-05-27 11:44:25 +0300

    feat: implement liteclient provider

--- src/config/CustomNetwork.ts
+++ src/config/CustomNetwork.ts
@@ -1,6 +1,6 @@
 export type CustomNetwork = {
-    endpoint: string;
-    version?: 'v2' | 'v4' | 'tonapi';
+    endpoint: string; // .
+    version?: 'v2' | 'v4' | 'tonapi' | 'liteclient';
     key?: string;
     type?: 'mainnet' | 'testnet' | 'custom';
 };
