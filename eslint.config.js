11d2549d3e23217bfd1d9d6a5f5042bb47defd33
Ilyar <ilyar.software@gmail.com>
2025-06-04 18:56:33 +0200

    chore: upgrade yarn

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
 const base = require('@ton/toolchain');
 const tsEslint = require('@ton/toolchain').tsEslint;
 
@@ -10,14 +9,10 @@ module.exports = [
             '@typescript-eslint': tsEslint,
         },
         rules: {
-            'import/order': 'warn',
-            'no-empty': 'warn',
-            'no-useless-escape': 'warn',
             'no-console': 'off',
             'no-redeclare': 'off',
             '@typescript-eslint/no-redeclare': ['error'],
             '@typescript-eslint/no-explicit-any': 'off',
-            '@typescript-eslint/no-require-imports': 'warn',
         },
     },
 ];
