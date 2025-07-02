772ac47e0da813eb40621635fba6887f030c19e6
Dev-Re2906 <jalilirad2906@gmail.com>
2025-07-02 16:53:59 +0330

    Add files via upload

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testPathIgnorePatterns: ['src/cli/test.ts'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ["/node_modules/","/dist/"],
+  maxWorkers: 1
 };
